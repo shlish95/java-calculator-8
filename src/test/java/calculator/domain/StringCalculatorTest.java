c9abf23a4916632aab87b696c0c61a999b3db727
shlihs95 <fhzptvh@gmail.com>
2025-10-20 20:20:24 +0900

    test: 기본 구분자(콤마/콜론) 합산 테스트 추가

--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -11,5 +11,11 @@ class StringCalculatorTest {
         assertEquals(0, StringCalculator.add(null));
     }
 
+    @Test
+    void 기본구분자_콤마_콜론으로_합산한다() {
+        assertEquals(3, StringCalculator.add("1,2"));
+        assertEquals(6, StringCalculator.add("1,2,3"));
+        assertEquals(6, StringCalculator.add("1,2:3"));
+    }
 
 }
